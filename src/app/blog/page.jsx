f745b2ad0cbe6b992495471490964ee000f9c813
Ometere <ookuoluse@yahoo.com>
2023-07-01 07:54:56 +0100

    blog post edited

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -7,21 +7,23 @@ export default function Blog() {
   return (
     <div className="flex flex-col items-center gap-[50px] mb-[50px]">
       {blogs.map((blog) => (
-        <Link href={blog.url} className="flex items-center gap-7">
-          <div key={blog.id}>
-            <Image
-              src={blog.image}
-              alt=""
-              width={400}
-              height={250}
-              className="object-cover"
-            />
-          </div>
-          <div>
-            <h1>{blog.heading}</h1>
-            <p>{blog.description}</p>
-          </div>
-        </Link>
+        <div key={blog.id}>
+          <Link href={blog.url} className="flex items-center gap-7">
+            <div>
+              <Image
+                src={blog.image}
+                alt=""
+                width={400}
+                height={250}
+                className="object-cover"
+              />
+            </div>
+            <div>
+              <h1>{blog.heading}</h1>
+              <p>{blog.description}</p>
+            </div>
+          </Link>
+        </div>
       ))}
     </div>
   );
